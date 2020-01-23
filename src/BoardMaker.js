22c2590549826db59777ef5e19f102b3e840d1fb
burningmagic <burningmagic@nexon.co.kr>
2020-01-23 18:17:53 +0900

    모달 띄움

--- src/BoardMaker.js
+++ src/BoardMaker.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import Board from './Board'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class BoardMaker extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            createModalShow: false
+        }
+    }
+
     render(){
+        const handleShow = () => this.setState({
+            createModalShow: true
+        });
+        const handleClose = () => this.setState({
+            createModalShow: false
+        });
+        
         return (
             <div className="global-container">
                 <div className="global-buttons">
@@ -15,9 +31,23 @@ class BoardMaker extends React.Component {
                 </div>
                 <div className="board-maker-buttons">
                     <ButtonToolbar>
-                        <Button variant="outline-primary">Create a new board...</Button>
+                        <Button variant="outline-primary" onClick={handleShow}>Create a new board...</Button>
                     </ButtonToolbar>
                 </div>
+                <Modal show={this.state.createModalShow} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
 
